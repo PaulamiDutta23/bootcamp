c0ebd931f9edb8dd22abf9fb8b75eadec903c1f5
Paulami Dutta <230529792+PaulamiDutta23@users.noreply.github.com>
2026-05-07 11:05:58 +0530

    Implements initial setup

---
+++ src/com/bootcamp/problem1/App.java
@@ -0,0 +1,4 @@
+package com.bootcamp.problem1;
+
+public class App {
+}
---
+++ test/com/bootcamp/problem1/AppTest.java
@@ -0,0 +1,14 @@
+package com.bootcamp.problem1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void mockTest() {
+        assertEquals(1,1);
+    }
+
+}
